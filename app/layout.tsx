aebd3aaf982f59230a44538ff4244f8a078b51a5
Febrian Bayu Nugroho <fbrnngrh@gmail.com>
2023-11-02 22:53:19 +0800

    update-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263] "></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]  md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394] "></div>
+        <div className="bg-[#dbd7fb] absolute bottom-[5rem] left-[-35rem] h-[31.25rem] -z-10 w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem]  md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394] "></div>
+        <div className="bg-[#dbd7fb] absolute bottom-[6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#676394] "></div>
         <ThemeContextProvider>
           <ActiveSectionContextProfider>
             <Header />
